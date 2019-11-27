c7c91dfc6ab357a14965d2bc1301354196042c8b
Ayaz Akram <yazakram@ucdavis.edu>
2019-11-26 22:30:01 -0800

    Updating things to support running m5 calls in kvm mode

--- NPB3.3.1/NPB3.3-OMP/common/hooks.c
+++ NPB3.3.1/NPB3.3-OMP/common/hooks.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "m5_mmap.h"
 
 void roi_begin_(){
+
+	// need to mmap /dev/mem
+	map_m5_mem();
 	printf(" -------------------- ROI BEGIN -------------------- \n");
 	m5_dump_reset_stats(0,0);
 	}
