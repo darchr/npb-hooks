7145860da6d6dc6a6e9e34473464e007b896111e
Ayaz Akram <yazakram@ucdavis.edu>
2019-11-28 15:46:50 -0800

    replacing dumpstats calls with m5 exit calls in hooks.c
    This helps to have more control on things that can be done
    around an ROI (from gem5 run script)

--- NPB3.3.1/NPB3.3-OMP/common/hooks.c
+++ NPB3.3.1/NPB3.3-OMP/common/hooks.c
@@ -6,10 +6,10 @@ void roi_begin_(){
 	// need to mmap /dev/mem
 	map_m5_mem();
 	printf(" -------------------- ROI BEGIN -------------------- \n");
-	m5_dump_reset_stats(0,0);
+	m5_exit(0);
 	}
 
 void roi_end_(){
        	printf(" -------------------- ROI END -------------------- \n");
-	m5_dump_reset_stats(0,0);
+	m5_exit(0);
 	}
